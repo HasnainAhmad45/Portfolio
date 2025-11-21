305ab2e0e3bd06f4d41b39f8f70853887b12a809
Hasnain Ahmad <hasnainirshad44@gmail.com>
2025-11-21 19:10:30 +0500

    Changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { render } from "react-snapshot";
+import ReactDOM from "react-dom/client";
 import { App } from "./app/App";
 import "./index.css";
 
-render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
